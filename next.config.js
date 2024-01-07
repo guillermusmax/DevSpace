c687395e42556aa324d836eb66ff49773637242d
KelvinG09 <108686606+KelvinG09@users.noreply.github.com>
2024-01-07 15:02:58 -0400

    fixing package json

--- next.config.js
+++ next.config.js
@@ -1,20 +1,16 @@
-const { GitHub } = require('@mui/icons-material');
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+      domains: ['github.com'],
+  },
+  // async rewrites() {
+  //     return [
+  //         {
+  //             source: '/api/:path*',
+  //             destination: 'http://localhost:3001/api/:path*', // Replace with your Express API's URL
+  //         },
+  //     ];
+  // },
+}
 
 module.exports = nextConfig
-
-// next.config.js
-module.exports = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        pathname: '**',
-      },
-    ],
-  },
-}
-  
