964fc9696ba42281d4a6173f5990ed7b5301124a
KelvinG09 <108686606+KelvinG09@users.noreply.github.com>
2024-01-14 14:51:27 -0400

    paginas de proximamente en caso de que no se acabe a tiempo

---
+++ src/app/publicar/page.js
@@ -0,0 +1,17 @@
+"use client";
+
+export default function PROXIMAMENTE() {
+  const estiloDelFondo = {
+    backgroundImage: 'url("https://img.freepik.com/free-vector/coming-soon-text-abstract-sunrise-dark-background-with-motion-effect_157027-1073.jpg?w=1060&t=st=1705258193~exp=1705258793~hmac=c06164ae2c7c959afbee2b1bff4b91711c0f7a08cebdc3aa70694a3c870b412e")',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    height: '100vh', // Ajusta la altura según tus necesidades
+    // Otros estilos de posicionamiento o diseño según sea necesario
+  };
+
+  return (
+    <div style={estiloDelFondo}>
+      
+    </div>
+  );
+}
